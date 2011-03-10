a5f487b3fbada5dec85f44dafccf59d1d4c52250
Pepe Barbe <dev@antropoide.net>
2011-03-10 13:37:31 -0600

    bugfix

--- psutil/_pslinux.py
+++ psutil/_pslinux.py
@@ -524,7 +524,7 @@ class LinuxProcess(object):
                         conn = ntuple_connection(fd, family, _type, laddr,
                                                  raddr, status, 1)
                 else:
-                    _, count, _, _, _, status, inode = line.split()[:7]
+                    _, count, _, _, _, status, inode = line.split(None, 7)
                     path = line.rsplit(None, 1)[-1]
                     if path == inode:
                         # Some lines are missing a path. In that case the previous line should
